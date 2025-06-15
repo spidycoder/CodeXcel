99b8b9cd595305768933514402983fa32b2f7711
SpidyCoder <imh10028.21@bitmesra.ac.in>
2025-06-15 19:36:43 +0530

    made some chagnes

--- Frontend/Client/src/components/PrivateRoute.js
+++ Frontend/Client/src/components/PrivateRoute.js
@@ -1,10 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate,Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const user = JSON.parse(localStorage.getItem("user"));
-  const navigate = useNavigate();
   if (!user || !user.token) {
-    return navigate("/login");
+    return <Navigate to="/login"/>;
   }
 
   return children;
